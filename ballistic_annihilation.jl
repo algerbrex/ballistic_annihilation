a135013975a12edbad49e087ce646e75bee683e4
Christian Dean <algerbrex@gmail.com>
2023-07-02 19:51:52 -0400

    Add support for arrow collision generalization

--- ballistic_annihilation.jl
+++ ballistic_annihilation.jl
@@ -7,19 +7,35 @@ const RANGE_UPPER_BOUND          = 30000
 const RANGE_TO_COUNT_LOWER_BOUND = RANGE_LOWER_BOUND / 3
 const RANGE_TO_COUNT_UPPER_BOUND = RANGE_UPPER_BOUND / 3
 const NUM_OF_PARTICLES           = 5_000_000
-const PROBABILITY_OF_BLOCKADE    = 0.24
+const PROBABILITY_OF_BLOCKADE    = 0.3
 
-const A = 0
-const B = 0
-const C = 1 - (A + B)
+const PROBABILITY_ARROW_SURVIES         = .1
+const PROBABILITY_BLOCKADE_SURVIVES     = .9
+const PROBABILITY_OF_MUTAL_ANNIHILATION =  1 - (PROBABILITY_ARROW_SURVIES + PROBABILITY_BLOCKADE_SURVIVES)
+const ALPHA                             = 0.1
 
-const LEFT_VEL     = 0
-const RIGHT_VEL    = 1
-const BLOCKADE_VEL = 2
+const LEFT_VEL                = 0
+const RIGHT_VEL               = 1
+const BLOCKADE_VEL            = 2
 
 VELOCITIES            = [BLOCKADE_VEL, LEFT_VEL, RIGHT_VEL]
-ASSIGNMENT_WEIGHTS    = Weights([PROBABILITY_OF_BLOCKADE, (1 - PROBABILITY_OF_BLOCKADE) / 2, (1 - PROBABILITY_OF_BLOCKADE) / 2])
-COLLISION_WEIGHTS     = Weights([C, B, A])
+ASSIGNMENT_WEIGHTS = Weights([
+    PROBABILITY_OF_BLOCKADE, 
+    (1 - PROBABILITY_OF_BLOCKADE) / 2, 
+    (1 - PROBABILITY_OF_BLOCKADE) / 2
+])
+
+BLOCKADE_ARROW_COLLISION_WEIGHTS = Weights([
+    PROBABILITY_OF_MUTAL_ANNIHILATION, 
+    PROBABILITY_BLOCKADE_SURVIVES,
+    PROBABILITY_ARROW_SURVIES
+])
+
+ARROW_ARROW_COLLISION_WEIGHTS = Weights([
+    ALPHA / 2, # Right arrow survives
+    ALPHA / 2, # Left arrow survives
+    1 - ALPHA  # Both arrows are annihilated
+])
 
 
 function run_simulation() 
@@ -44,25 +60,27 @@ function run_simulation()
         
         while i <= num_left
             if i + 1 <= num_left && velocities[i] == RIGHT_VEL && velocities[i + 1] == LEFT_VEL
-                push!(indexes_to_delete, i, i + 1)
+                collision_result = sample([[i + 1], [i], [i, i + 1]], BLOCKADE_ARROW_COLLISION_WEIGHTS)
+                push!(indexes_to_delete, collision_result...)
                 i += 1
             elseif i - 1 >= 1 && velocities[i] == LEFT_VEL && velocities[i - 1] == RIGHT_VEL
-                push!(indexes_to_delete, i, i - 1)
+                collision_result = sample([[i], [i - 1], [i, i - 1]], BLOCKADE_ARROW_COLLISION_WEIGHTS)
+                push!(indexes_to_delete, collision_result...)
             elseif velocities[i] == BLOCKADE_VEL
                 if i + 1 <= num_left && i - 1 >= 1 && velocities[i - 1] == RIGHT_VEL && velocities[i + 1] == LEFT_VEL
                     if abs(positions[i - 1] - positions[i]) < abs(positions[i + 1] - positions[i])
-                        collision_result = sample([[i - 1, i], [i - 1], [i]], COLLISION_WEIGHTS)
+                        collision_result = sample([[i - 1, i], [i - 1], [i]], BLOCKADE_ARROW_COLLISION_WEIGHTS)
                         push!(indexes_to_delete, collision_result...)
                     else
-                        collision_result = sample([[i, i + 1], [i + 1], [i]], COLLISION_WEIGHTS)
+                        collision_result = sample([[i, i + 1], [i + 1], [i]], BLOCKADE_ARROW_COLLISION_WEIGHTS)
                         push!(indexes_to_delete, collision_result...)
                         i += 1
                     end
                 elseif i - 1 >= 1 && velocities[i - 1] == RIGHT_VEL
-                    collision_result = sample([[i - 1, i], [i - 1], [i]], COLLISION_WEIGHTS)
+                    collision_result = sample([[i - 1, i], [i - 1], [i]], BLOCKADE_ARROW_COLLISION_WEIGHTS)
                     push!(indexes_to_delete, collision_result...)
                 elseif i + 1 <= num_left && velocities[i + 1] == LEFT_VEL
-                    collision_result = sample([[i, i + 1], [i + 1], [i]], COLLISION_WEIGHTS)
+                    collision_result = sample([[i, i + 1], [i + 1], [i]], BLOCKADE_ARROW_COLLISION_WEIGHTS)
                     push!(indexes_to_delete, collision_result...)
                     i += 1
                 end
